03a070b853d825d3a86e767444bbcc8785f005b4
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-10-28 15:09:17 +0200

    Added get jokes function and fixed styling

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
 
+// function test(){
+//     VoiceRSS.speech({
+//         key: '065d884ddbf541c9a706704572d2c26b',
+//         src: 'Hello, world!',
+//         hl: 'en-us',
+//         v: 'Linda',
+//         r: 0,
+//         c: 'mp3',
+//         f: '44khz_16bit_stereo',
+//         ssml: false
+//     });
+// }
+// test();
 
-function test(){
-    VoiceRSS.speech({
-        key: '065d884ddbf541c9a706704572d2c26b',
-        src: 'Hello, world!',
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0, 
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
-    });
+async function getJokes() {
+  const apiUrl =
+    'https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Dark?blacklistFlags=nsfw,religious,political,racist,sexist';
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  if (data.setup) {
+    joke = `${data.setup}...${data.delivery}`;
+  } else {
+    joke = data.joke;
+  }
+
+  try {
+  } catch (error) {
+    console.log('Woops' + error);
+  }
 }
-
-test();
